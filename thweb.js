0fc225d0bf9ff2fe2d857bdc7569667c9cccc5e2
thurst0 <hurs4529@gmail.com>
2017-04-29 14:25:38 -0600

    Add files via upload

---
+++ thweb.js
@@ -0,0 +1,52 @@
+var thApp = angular.module('thApp', [])
+    
+thApp.controller('GlobalController', function GlobalController($scope, $rootScope, $controller) {
+	$scope.page = {
+		contact: false,
+		resume: false,
+		home: true
+	}
+	$scope.title = 
+	$scope.show = function(div){
+		console.log(div);
+		if(div=="home"){
+			$scope.title = "";
+			//dataWord();
+		}else if(div=="resume"){
+			$scope.title = "RESUME";
+		}else if(div=="Contact"){
+			$scope.title = "CONTACT";
+		}
+		$scope.page.contact = false;
+		$scope.page.resume = false;
+		$scope.page.home = false;
+		$scope.page[div] = true;
+	}
+});
+function dataWord () {
+
+  $("[data-words]").attr("data-words", function(i, d){
+    var $self  = $(this),
+        $words = d.split("|"),
+        tot    = $words.length,
+        c      = 0; 
+
+    // CREATE SPANS INSIDE SPAN
+    for(var i=0; i<tot; i++) $self.append($('<span/>',{text:$words[i]}));
+
+    // COLLECT WORDS AND HIDE
+    $words = $self.find("span").hide();
+
+    // ANIMATE AND LOOP
+    (function loop(){
+      $self.animate({ width: $words.eq( c ).width() });
+      $words.stop().fadeOut().eq(c).fadeIn().delay(1000).show(0, loop);
+      c = ++c % tot;
+    }());
+    
+  });
+
+}
+
+// dataWord(); // If you don't use external fonts use this on DOM ready; otherwise use:
+$(window).on("load", dataWord);
